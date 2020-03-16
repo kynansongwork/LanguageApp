974c44651a85994497890abfccdb95d4bc7a199d
Kynan Song <kynan.song@xdesign.com>
2020-03-16 14:05:05 +0000

    Created welcomeView

---
+++ LanguageApp/UI/Welcome/WelcomeView.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeView.swift
+//  LanguageApp
+//
+//  Created by Kynan Song on 16/03/2020.
+//  Copyright © 2020 xDesign. All rights reserved.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
