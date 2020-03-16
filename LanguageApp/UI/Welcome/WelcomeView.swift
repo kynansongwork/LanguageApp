7011825f24eea2902e38097fe01dda03e53667af
Kynan Song <kynan.song@xdesign.com>
2020-03-16 15:42:08 +0000

    Completed welcome view

--- LanguageApp/UI/Welcome/WelcomeView.swift
+++ LanguageApp/UI/Welcome/WelcomeView.swift
@@ -9,8 +9,43 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    var size: CGFloat = 60
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("welcome-background", bundle: nil)
+                .resizable()
+                .aspectRatio(1 / 1, contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+                .saturation(0.5)
+                .blur(radius: 5)
+                .opacity(0.08)
+            HStack {
+                Image(systemName: "table")
+                    .resizable()
+                    .frame(width: size, height: size, alignment: .center)
+                    .background(Color(white: 0.9))
+                    .clipShape(Circle())
+                    .foregroundColor(.red)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: size / 2)
+                            .stroke(Color.gray, lineWidth: 1)
+                        )
+                
+                VStack {
+                    Text("Welcome to")
+                        .font(.headline)
+                        .bold()
+                    Text("Language App")
+                        .font(.largeTitle)
+                        .bold()
+                }
+                .foregroundColor(.red)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+                .padding(.horizontal)
+            }
+        }
     }
 }
 
